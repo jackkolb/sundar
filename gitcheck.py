c1a1d1cbde4ca5b2f570e38f148f6ddefeda869c
Jack Kolb <jackhunterkolb@gmail.com>
2018-05-21 10:39:54 -0700

    started update check, freeze for reconfiguration of the url (from https to SSH)

--- gitcheck.py
+++ gitcheck.py
@@ -1,8 +1,11 @@
 import subprocess
 
+
 # checks for new code every 10 minutes, if there is an update, pull it and restart processes
 def git_check_loop():
-    result = subprocess.Popen("", shell=True, stdout=subprocess.PIPE).stdout.read()
-    print(result)
+    while True:
+        result = subprocess.Popen("echo hello", shell=True, stdout=subprocess.PIPE).stdout.read()
+        print(result)
+
 
 git_check_loop()
