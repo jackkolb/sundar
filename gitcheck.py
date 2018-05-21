5ad83d993dfd84f4a121ef11f7289fe1f19cc471
Jack Kolb <jackhunterkolb@gmail.com>
2018-05-21 14:25:18 -0700

    added the git check system, and initializer in main.py

--- gitcheck.py
+++ gitcheck.py
@@ -1,11 +1,21 @@
 import subprocess
+import time
+import sys
+import logs
 
 
 # checks for new code every 10 minutes, if there is an update, pull it and restart processes
 def git_check_loop():
     while True:
-        result = subprocess.Popen("echo hello", shell=True, stdout=subprocess.PIPE).stdout.read()
-        print(result)
+        result = str(subprocess.Popen("git pull", shell=True, stdout=subprocess.PIPE).stdout.read())
+        result = result[2:-3]
 
+        if result == "Already up to date.":  # latest remote commit matches current
+            pass
 
-git_check_loop()
+        else:  # new code on GitHub
+            # reset the main script
+            logs.log("[Git Check] New build found on GitHub, resetting")
+            sys.exit()
+
+        time.sleep(60 * 10)  # waits 10 minutes
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-
+import threading
+import gitcheck
 
 def main():
     print("Woo!")
+    # launch git check thread -- checks if there are code updates, if there are it kills the program
+    git_check_thread = threading.Thread(target=gitcheck.git_check_loop)
+    git_check_thread.start()
+    print("started")
     return
 
 
