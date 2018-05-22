090e1393b99db8713e93742c6da76bfe1417f950
Jack Kolb <jackhunterkolb@gmail.com>
2018-05-21 17:26:02 -0700

    setup for the general loop

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import threading
 import gitcheck
-
+import time
+import datetime
 
 def main():
     print("Woo!")
@@ -8,4 +9,14 @@ def main():
     git_check_thread = threading.Thread(target=gitcheck.git_check_loop)
     git_check_thread.start()
     print("started")
+
+    # general loop: polls sensors, stores to logs, uploads at midnight
+    while True:
+        current_time = datetime.datetime.now()
+
+
+        time.sleep(5)
+
+
+
     return
