01daf35d4dbbd2ce1f14019d7f87d635b66accc7
Jack Kolb <jackhunterkolb@gmail.com>
2018-09-20 15:24:24 -0700

    fixing loader so it resets scripts

--- loader.py
+++ loader.py
@@ -1,10 +1,17 @@
 import threading
-import subprocess
+import src.main
+import src.logs
+
 
 if __name__ == "__main__":
-    main_thread = threading.Thread(target=src.main.main)
-
+    
     while True:
+    	reload(src.main)
+    	reload(src.logs)
+    	import src.main
+    	import src.logs
+    	main_thread = threading.Thread(target=src.main.main)
         src.logs.log("[LOADER] Starting Main Thread")
-        subprocess.check_call(["python3", "./src/main.py"], shell=True)
+        main_thread.start()
+        main_thread.join()
         src.logs.log("[LOADER] Main Thread Ended")
