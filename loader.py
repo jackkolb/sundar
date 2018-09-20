221a14cc61a23c081d63330af5930a33771a1fca
Jack Kolb <jackhunterkolb@gmail.com>
2018-09-20 15:27:39 -0700

    tab->space

--- loader.py
+++ loader.py
@@ -6,11 +6,11 @@ import src.logs
 if __name__ == "__main__":
     
     while True:
-    	reload(src.main)
-    	reload(src.logs)
-    	import src.main
-    	import src.logs
-    	main_thread = threading.Thread(target=src.main.main)
+        reload(src.main)
+        reload(src.logs)
+        import src.main
+        import src.logs
+        main_thread = threading.Thread(target=src.main.main)
         src.logs.log("[LOADER] Starting Main Thread")
         main_thread.start()
         main_thread.join()
