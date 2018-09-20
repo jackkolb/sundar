14878099cf0f8f57a66633a7d535402152a81327
Jack Kolb <jackhunterkolb@gmail.com>
2018-09-20 14:17:43 -0700

    trying to fix accelerometer led

--- loader.py
+++ loader.py
@@ -1,12 +1,14 @@
 import threading
-import src.main
-import src.logs
+
 
 
 if __name__ == "__main__":
     main_thread = threading.Thread(target=src.main.main)
 
     while True:
+    	import src.main
+    	import src.logs
+
         src.logs.log("[LOADER] Starting Main Thread")
         main_thread.start()
         main_thread.join()
