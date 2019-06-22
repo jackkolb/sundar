409addc258a5f6dc604b3e5b434ebc5b98ff65c1
Jack Kolb <jackhunterkolb@gmail.com>
2019-06-21 19:43:08 -0700

    moved old C code, new main.c uses solo polls

--- .old/c/main.fifo.c
+++ .old/c/main.fifo.c
@@ -1,5 +1,5 @@
 // main.c, adapted from github.com/OrbitalBoson/
-
+// THIS CODE IS ARCHIVED, DO NOT BRING TO PRODUCTION UNLESS YOU KNOW WHAT YOU ARE DOING
 #include <unistd.h>
 #include <stdio.h>
 #include <dirent.h>
@@ -10,6 +10,8 @@
 #include "adxl355.h"
 
 FILE * fp;
+int counter = 0;
+int limit = 5000000;
 
 unsigned long long getMicrotime() {
   struct timeval tm;
@@ -17,36 +19,35 @@ unsigned long long getMicrotime() {
   return ((long long int) tm.tv_sec) * 1000000 + tm.tv_usec;
 }
 
+void my_callback(ADXL355Fifo * fifo)
+{
+  fprintf(fp, "%lld,", getMicrotime());
+  fprintf(fp, "%d,", fifo->data[0].x);
+  fprintf(fp, "%d,", fifo->data[0].y);
+  fprintf(fp, "%d\n", fifo->data[0].z);
+ }
+
 int main(int argc, char * argv[])
 {
   printf("                        [ADXL] Collecting\n");
   uint8_t t1 = 0b00010000;
   ADXL355_HANDLER spi = {};
+  //ADXL355Status status = {};
+  //ADXL355Temperature temp = {};
   spi.spi = 1;
   spi.spi_channel = 0;
   spi.speed = 2000000;
   wiringPiSetup();
   int res = wiringPiSPISetup(spi.spi_channel, spi.speed);
+  //ADXL355Command cmd;
+  //uint8_t r;
+  //ADXL355Fifo fifo = {};
+  //ADXL355Acceleration acc = {};
   adxl355_measurement_mode(&spi);
-
-  ADXL355Acceleration acc = {};
-
+  
   fp = fopen(argv[1], "a");
 
-  int sample_duration = 10; // sample time duration in seconds
-  int sample_rate = 1000; // sample rate
-  int samples = sample_duration * sample_rate;
-
-  for (int i = 0; i < 20; i++){
-    adxl355_read_acceleration(&spi, &acc);
-    fprintf(fp, "%lld,", getMicrotime());
-    fprintf(fp, "%d,", fifo->acc.x);
-    fprintf(fp, "%d,", fifo->acc.y);
-    fprintf(fp, "%d\n", fifo->acc.z);
-    usleep((1.0 / sample_rate) * 1000000);
-  }
-
-  printf("Done reading\n");
+  adxl355_fifo_stream(&spi, my_callback, FIFO_STREAM_OVR_BREAK|FIFO_STREAM_FIFO_READ_BREAK);
 
   fclose(fp);
   adxl355_standby_mode(&spi);
