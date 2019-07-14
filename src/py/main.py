7d901d49f9056137b54fa285a43add7a55e2a008
Jack Kolb <jackhunterkolb@gmail.com>
2019-07-14 16:28:16 -0700

    fixed imports (I hope)

--- src/py/main.py
+++ src/py/main.py
@@ -1,41 +1,41 @@
 import subprocess
 import threading
 import py.logs
-import settings
+import py.settings
 import RPi.GPIO as GPIO
 import os
-import poll.lcd
-import poll.led
-import poll.location
-import poll.gitcheck
-import poll.webserver
+import py.poll.lcd
+import py.poll.led
+import py.poll.location
+import py.poll.gitcheck
+import py.poll.webserver
 
 def main():
     GPIO.setmode(GPIO.BCM)
-    logs.log("[MAIN] Starting GitHub check thread")
+    py.logs.log("[MAIN] Starting GitHub check thread")
 
     # launch git check thread -- checks if there are code updates, if there are it kills the program
-    git_check_thread = threading.Thread(target=poll.gitcheck.git_check_loop)
+    git_check_thread = threading.Thread(target=py.poll.gitcheck.git_check_loop)
     git_check_thread.start()
 
     # start LCD thread
-    logs.log("[MAIN] Starting LCD thread")
-    lcd_thread = threading.Thread(target=poll.lcd.start_lcd)
+    py.logs.log("[MAIN] Starting LCD thread")
+    lcd_thread = threading.Thread(target=py.poll.lcd.start_lcd)
     lcd_thread.start()
 
     # start LED thread
-    logs.log("[MAIN] Starting LED thread")
-    led_thread = threading.Thread(target=poll.led.start_led)
+    py.logs.log("[MAIN] Starting LED thread")
+    led_thread = threading.Thread(target=py.poll.led.start_led)
     led_thread.start()
 
     # start webserver manager thread
-    logs.log("[MAIN] Starting Webserver Manager thread")
-    webserver_thread = threading.Thread(target=poll.webserver.manage_webserver)
+    py.logs.log("[MAIN] Starting Webserver Manager thread")
+    webserver_thread = threading.Thread(target=py.poll.webserver.manage_webserver)
     webserver_thread.start()
 
     # start location determination thread
-    logs.log("[MAIN] Starting output location thread")
-    location_thread = threading.Thread(target=poll.location.manage_output_location)
+    py.logs.log("[MAIN] Starting output location thread")
+    location_thread = threading.Thread(target=py.poll.location.manage_output_location)
     location_thread.start()
 
 
@@ -47,18 +47,18 @@ def main():
         except:
             status == "false"
         if status == "true":
-            destination = poll.location.get_output_location()
+            destination = py.poll.location.get_output_location()
             if destination == "none":
                 destination = "data/"
             accelerometer_destination = destination + "accelerometer.data"
-            logs.log("[MAIN] Starting accelerometer collection: " + accelerometer_destination)
-            sensor_process = subprocess.Popen(["./src/c/collect.o", settings.get_duration(), settings.get_rate(), accelerometer_destination])
+            py.logs.log("[MAIN] Starting accelerometer collection: " + accelerometer_destination)
+            sensor_process = subprocess.Popen(["./src/c/collect.o", py.settings.get_duration(), py.settings.get_rate(), accelerometer_destination])
             sensor_process.wait()  # wait for sensor thread to end, restart it if it does
-            logs.log("[MAIN] Collection complete")
-            logs.log("[MAIN] Starting classification")
+            py.logs.log("[MAIN] Collection complete")
+            py.logs.log("[MAIN] Starting classification")
             classifier_destination = destination + "classifier.data"
             # RUN CLASSIFICATION FUNCTION HERE
-            logs.log("[MAIN] Classification completed")
+            py.logs.log("[MAIN] Classification completed")
 
-        if poll.gitcheck.check_flag_file() == "RESET":
+        if py.poll.gitcheck.check_flag_file() == "RESET":
             os._exit(1)
