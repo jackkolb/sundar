60d07cf25dc1cddf257623f1be0db777ab144cd6
Jack Kolb <jackhunterkolb@gmail.com>
2019-08-12 21:16:45 -0700

    added variable unpacks for entry

--- src/py/logs.py
+++ src/py/logs.py
@@ -56,10 +56,38 @@ def generate_log_name():
     filename = "log_" + "pi" "_" + str(year) + "_" + str(month) + "_" + str(day)    
     return filename
 
+def generate_log_variables():
+    now = datetime.datetime.now()
+    year = now.year
+
+    month = now.month
+    if month < 10:
+        month = "0" + str(month)
+
+    day = now.day
+    if day < 10:
+        day = "0" + str(day)
+
+    hour = now.hour
+    if hour < 10:
+        hour = "0" + str(hour)
+
+    minute = now.minute
+    if minute < 10:
+        minute = "0" + str(minute)
+
+    second = now.second
+    if second < 10:
+        second = "0" + str(second)
+
+    return year, month, day, hour, minute, second
+
 
 # appends a message to the day's log file
 def log(module, message):
     filename = "logs/" + generate_log_name()
+
+    year, month, day, hour, minute, second = generate_log_variables()
 
     entry_time = "[ " + str(year) + "-" + str(month) + "-" + str(day)\
                  + " " + str(hour) + ":" + str(minute) + ":" + str(second) + " ]"
