374b9278ad4cdad9fd067c40452f42f1a9e883b4
Jack Kolb <jackhunterkolb@gmail.com>
2019-09-30 12:32:52 -0700

    file paths work, now sorting file names

--- src/www/node/information.py
+++ src/www/node/information.py
@@ -125,10 +125,10 @@ def load_settings():
 
 # gets all the log file names
 def load_all_logs():
-    log_list = os.listdir("logs/")
+    log_list = sort(os.listdir("logs/"))
     return log_list
 
 # gets all the log file names
 def load_all_data():
-    log_list = os.listdir("data/raw/")
+    log_list = sort(os.listdir("data/raw/"))
     return log_list
